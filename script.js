74079eea74b50b3fb82ade7bd698d7f01ce496c1
Barun Debnath <barundebnath91@gmail.com>
2022-09-25 17:00:02 +0530

    styling and layout done

--- script.js
+++ script.js
@@ -1,45 +1,59 @@
-const addBox = document.querySelector(".add-box"),
-    popupBox = document.querySelector(".popup-box"),
-    popupTitle = popupBox.querySelector("header p"),
-    closeIcon = popupBox.querySelector("header i"),
-    titleTag = popupBox.querySelector("input"),
-    descTag = popupBox.querySelector("textarea"),
-    addBtn = popupBox.querySelector("button");
+// import showMenu from "./showMenu";
 
-const months = ["January", "February", "March", "April", "May", "June", "July",
-    "August", "September", "October", "November", "December"];
+const addBox = document.querySelector(".add-box"),
+  popupBox = document.querySelector(".popup-box"),
+  popupTitle = popupBox.querySelector("header p"),
+  closeIcon = popupBox.querySelector("header i"),
+  titleTag = popupBox.querySelector("input"),
+  descTag = popupBox.querySelector("textarea"),
+  addBtn = popupBox.querySelector("button");
+
+const months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
 const notes = JSON.parse(localStorage.getItem("notes") || "[]");
-let isUpdate = false, updateId;
+let isUpdate = false,
+  updateId;
 
 addBox.addEventListener("click", () => {
-    popupTitle.innerText = "Write your Thoughts";
-    addBtn.innerText = "Create";
-    popupBox.classList.add("show");
-    document.querySelector("body").style.overflow = "hidden";
-    if (window.innerWidth > 660) titleTag.focus();
+  popupTitle.innerText = "Write your Thoughts";
+  addBtn.innerText = "Create";
+  popupBox.classList.add("show");
+  document.querySelector("body").style.overflow = "hidden";
+  if (window.innerWidth > 660) titleTag.focus();
 });
 
 // To close note
 closeIcon.addEventListener("click", () => {
-    isUpdate = false;
-    titleTag.value = descTag.value = "";
-    popupBox.classList.remove("show");
-    document.querySelector("body").style.overflow = "auto";
+  isUpdate = false;
+  titleTag.value = descTag.value = "";
+  popupBox.classList.remove("show");
+  document.querySelector("body").style.overflow = "auto";
 });
 
 // To make bold text
 
-
 // To Copy the entire text
 
-// To 
+// To
 
 function showNotes() {
-    if (!notes) return;
-    document.querySelectorAll(".note").forEach(li => li.remove());
-    notes.forEach((note, id) => {
-        let filterDesc = note.description.replaceAll("\n", '<br/>');
-        let liTag = `<li class="note">
+  if (!notes) return;
+  document.querySelectorAll(".note").forEach((li) => li.remove());
+  notes.forEach((note, id) => {
+    let filterDesc = note.description.replaceAll("\n", "<br/>");
+    let liTag = `<li class="note">
                         <div class="details">
                             <p>${note.title}</p>
                             <span>${filterDesc}</span>
@@ -55,66 +69,60 @@ function showNotes() {
                             </div>
                         </div>
                     </li>`;
-        addBox.insertAdjacentHTML("afterend", liTag);
-    });
+    addBox.insertAdjacentHTML("afterend", liTag);
+  });
 }
-
-
-
-
 
 showNotes();
 
 function showMenu(elem) {
-    elem.parentElement.classList.add("show");
-    document.addEventListener("click", e => {
-        if (e.target.tagName != "I" || e.target != elem) {
-            elem.parentElement.classList.remove("show");
-        }
-    });
+  elem.parentElement.classList.add("show");
+  document.addEventListener("click", (e) => {
+    if (e.target.tagName != "I" || e.target != elem) {
+      elem.parentElement.classList.remove("show");
+    }
+  });
 }
 
 function deleteNote(noteId) {
-    let confirmDel = confirm("Are you sure you want to delete this note?");
-    if (!confirmDel) return;
-    notes.splice(noteId, 1);
-    localStorage.setItem("notes", JSON.stringify(notes));
-    showNotes();
+  let confirmDel = confirm("Are you sure you want to delete this note?");
+  if (!confirmDel) return;
+  notes.splice(noteId, 1);
+  localStorage.setItem("notes", JSON.stringify(notes));
+  showNotes();
 }
 
 function updateNote(noteId, title, filterDesc) {
-    let description = filterDesc.replaceAll('<br/>', '\r\n');
-    updateId = noteId;
-    isUpdate = true;
-    addBox.click();
-    titleTag.value = title;
-    descTag.value = description;
-    popupTitle.innerText = "Update your Note";
-    addBtn.innerText = "Update!";
+  let description = filterDesc.replaceAll("<br/>", "\r\n");
+  updateId = noteId;
+  isUpdate = true;
+  addBox.click();
+  titleTag.value = title;
+  descTag.value = description;
+  popupTitle.innerText = "Update your Note";
+  addBtn.innerText = "Update!";
 }
 
-addBtn.addEventListener("click", e => {
-    e.preventDefault();
-    let title = titleTag.value.trim(),
-        description = descTag.value.trim();
+addBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  let title = titleTag.value.trim(),
+    description = descTag.value.trim();
 
-    if (title || description) {
-        let currentDate = new Date(),
-            month = months[currentDate.getMonth()],
-            day = currentDate.getDate(),
-            year = currentDate.getFullYear();
+  if (title || description) {
+    let currentDate = new Date(),
+      month = months[currentDate.getMonth()],
+      day = currentDate.getDate(),
+      year = currentDate.getFullYear();
 
-        let noteInfo = { title, description, date: `${month} ${day}, ${year}` }
-        if (!isUpdate) {
-            notes.push(noteInfo);
-        } else {
-            isUpdate = false;
-            notes[updateId] = noteInfo;
-        }
-        localStorage.setItem("notes", JSON.stringify(notes));
-        showNotes();
-        closeIcon.click();
+    let noteInfo = { title, description, date: `${month} ${day}, ${year}` };
+    if (!isUpdate) {
+      notes.push(noteInfo);
+    } else {
+      isUpdate = false;
+      notes[updateId] = noteInfo;
     }
+    localStorage.setItem("notes", JSON.stringify(notes));
+    showNotes();
+    closeIcon.click();
+  }
 });
-
-
